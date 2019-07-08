fce23e83c4e4665e56b6b0f7c07fae72f3b343ce
s2arora <s2arora@s2aroraui-MacBook-Pro.local>
2019-07-08 18:20:37 +0900

    H.W

---
+++ homework3.py
@@ -0,0 +1,20 @@
+seq = str(input('염기 한글자를 써줘: '))
+'''
+A = 'Adenine'
+C = 'Cytosine'
+G = 'Guanine'
+T = 'Thymine'
+U = 'Uracil'
+'''
+if seq == 'A' :
+	print ('Adenine')
+elif seq == 'C' :
+	print ('Cytosine')
+elif seq == 'G' :
+	print ('Guanine')
+elif seq == 'T' :
+	print ('Thymine')
+elif seq == 'U' :
+	print ('Uracill')
+
+
---
+++ homework4.py
@@ -0,0 +1,5 @@
+try:
+	n = input('input number: ')
+	print (10/int(n))
+except ZeroDivisionError:	
+	print('0으로는 나눌수 없어요') 
